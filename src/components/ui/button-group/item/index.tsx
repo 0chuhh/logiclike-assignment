fcbec50016d14b937bb841be799147ede48f31fb
Viktor <vvlasovdo@mail.ru>
2024-05-31 16:14:34 +0900

    ref: button group type to generic

--- src/components/ui/button-group/item/index.tsx
+++ src/components/ui/button-group/item/index.tsx
@@ -1,20 +1,21 @@
-import React, { FC, PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 import styles from './styles.module.scss';
 
-export interface IButtonGroupItemProps extends PropsWithChildren {
+export interface IButtonGroupItemProps<T> extends PropsWithChildren {
     className?: string;
     selected?: boolean;
-    value: string | number;
-    onClick?: (value: IButtonGroupItemProps['value']) => void;
+    value: T;
+    onClick?: (value: T) => void;
 }
 
-export const ButtonGroupItem: FC<IButtonGroupItemProps> = React.memo(({ value, onClick, className, selected = false, children }) => {
+export const ButtonGroupItem = <T,>({ value, onClick, className, selected = false, children }: IButtonGroupItemProps<T>) => {
     const handleClick = () => {
         onClick?.(value);
     };
+
     return (
         <div onClick={handleClick} className={[selected ? styles.item_selected : styles.item, className].join(' ')}>
             {children}
         </div>
     );
-});
+};
